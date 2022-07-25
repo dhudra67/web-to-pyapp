1671cf90de107d804ba2e4e1c37ec6da5d0e1e45
dhudra67 <muhammadfaizanali67@gmil.com>
2022-07-25 22:18:27 +0500

    Initial Commit

--- tk-0.py
+++ tk-0.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import webview
 root = tk.Tk()
-root.title("First Program")
+root.title("Hd Today")
 root.geometry('1400x900+50+50')
 webview.create_window('HD SERIES' , 'https://hdtoday.tv/home')
 webview.start()
